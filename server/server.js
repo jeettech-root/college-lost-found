df76965e6be82a85a1665ac02c49e60ae9f005b2
Karan Mehta <mehtakaran165@gmail.com>
2026-08-08 15:34:04 +0530

    Solving register/login

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const lostRoutes = require('./routes/lostRoutes');
 
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 
